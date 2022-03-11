1f68d14c482ee40727b216fbb6dd808cdd79c9d9
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-11 20:16:55 +0300

    added methods to verify passwords, save and delete users

--- app/models.py
+++ app/models.py
@@ -10,3 +10,27 @@ class User(db.Model, UserMixin):
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
     upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
     downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
+
+    def is_active(self):
+        return True
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @set_password.setter
+    def password(self, password):
+        self.secure_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password,password) 
+    
+    def save_u(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def __repr__(self):
+        return f'User {self.username}'
