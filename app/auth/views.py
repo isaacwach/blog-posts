3324a7c767db54ac8a8480f8224f3031dc1abe11
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-11 22:44:06 +0300

    created login and logout routes

--- app/auth/views.py
+++ app/auth/views.py
@@ -18,3 +18,21 @@ def signup():
         #mail_message("Welcome to Spicy pitches","email/welcome_user",user.email,user=user)
         return redirect(url_for('main.index'))
     return render_template('auth/signup.html', reg_form = form)
+
+
+@auth.route('/login', methods = ['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user != None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or Password')
+    return render_template('auth/login.html', loginform = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
