4c2f13208084b44ad47c4d2bbdc3fd7583965c15
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-11 23:10:03 +0300

    created the index route in views

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,16 @@
-from flask import render_template
-from . import main
+from flask import render_template, redirect, request, url_for, abort
+from . import main 
+from flask_login import login_required, current_user
+from .. import db,photos
+from .forms import ProfileUpdate,PitchForm,CommentForm
+from ..models import User,Pitch,Comment,Upvote,Downvote
 
 @main.route('/')
 def index():
-    return '<h1>Welcom to zac-master blog</h1>'
+    pitches = Pitch.query.all()
+    motivation = Pitch.query.filter_by(category = 'Motivation').all() 
+    jokes = Pitch.query.filter_by(category = 'Jokes').all()
+    education = Pitch.query.filter_by(category = 'Education').all()
+
+    return render_template('index.html', pitches=pitches, education=education, motivation=motivation, jokes=jokes)
+    
