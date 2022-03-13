d6a2cab4456915eabed16da4b92f49e99d03f31c
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-13 22:37:29 +0300

    deployment

---
+++ migrations/versions/70efc8a6cca0_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 70efc8a6cca0
+Revises: 4e7e500639f0
+Create Date: 2022-03-13 22:15:46.252064
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '70efc8a6cca0'
+down_revision = '4e7e500639f0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
