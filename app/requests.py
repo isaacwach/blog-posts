93d72639982709c1d29ab2bf4d04cec65c2d5b0a
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-13 17:34:59 +0300

    installed flask migrate

--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,8 @@
 import requests,json
+import time
+
+url = 'http://quotes.stormconsultancy.co.uk/random.json'   
+page = requests.get(url, verify=False)
 
 def get_quotes():
     response = requests.get('http://quotes.stormconsultancy.co.uk/random.json')
