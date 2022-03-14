7710a95721e4102db9250eb103b4a4175498c76e
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-14 16:03:53 +0300

    reinstalled flask migrations

---
+++ migrations2/versions/533fe3b4f28d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 533fe3b4f28d
+Revises: 62febcc1e79d
+Create Date: 2022-03-14 12:47:50.470865
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '533fe3b4f28d'
+down_revision = '62febcc1e79d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
